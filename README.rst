7a64260afbed008dbe71ea4fee2a35427947c228
Milton <madandat@gmail.com>
2014-02-24 12:03:59 +0200

    added config files

--- README.rst
+++ README.rst
@@ -1,4 +1,16 @@
-url-shortening-service
-======================
+URL SHORTERNING SERVICE
+=======================
 
 An API service to shorten URLs
+
+|travis|_ |coveralls|_
+
+Install
+~~~~~~~
+
+::
+
+    $ virtualenv ve
+    $ source ve/bin/activate
+    (ve)$ pip install -e .
+    (ve)$ trial shortener
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+    name="shortener",
+    version="0.1",
+    url='http://github.com/praekelt/url-shortening-service',
+    license='BSD',
+    description="An API service to shorten URLs",
+    long_description=open('README.rst', 'r').read(),
+    author='Praekelt',
+    author_email='dev@praekelt.com',
+    packages=[
+        "shortener",
+    ],
+    package_data={},
+    include_package_data=True,
+    install_requires=[
+        'Twisted',
+        'aludel',
+        'treq',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Networking',
+        'Framework :: Twisted',
+    ],
+)
