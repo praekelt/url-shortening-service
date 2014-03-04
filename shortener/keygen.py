6ae72a9796aeadf2166a4043e84d1f19f557ae1e
Jeremy Thurgood <jeremy@praekelt.com>
2014-03-04 14:48:28 +0200

    Explanatory docstring for shuffler.

--- shortener/keygen.py
+++ shortener/keygen.py
@@ -7,14 +7,27 @@ SHUFFLE_SEED = 1234
 
 
 def shuffle(items):
+    """
+    Reorder a sequence deterministically.
+
+    A pseudo-random number generator is initialised with a constant seed value
+    and used to sample all elements in the sequence.
+
+    :class:`random.Random` is a deterministic Mersenne Twister implementation
+    and will always return the same sequence of values when initialised with
+    the same seed across all Python versions (CPython and the equivalent pypy
+    implementations) from 2.3 to at least 3.3. Python 3 documentations asserts
+    that if the pseudo-random number generator changes in a future release, a
+    backward-compatible implementation will be made available.
+    """
     return random.Random(SHUFFLE_SEED).sample(items, len(items))
 
 
 def generate_token(counter, alphabet=DEFAULT_ALPHABET):
-    '''
+    """
     Generates a short url token using the given counter from the alphabet
     min_length: 3 (using SHORT_URL_OFFSET)
-    '''
+    """
     if not isinstance(counter, int):
         raise TypeError('an integer is required')
 
